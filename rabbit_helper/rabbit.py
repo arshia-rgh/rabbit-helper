28c1dd2d91d89674b43d8870415a739ad332ec77
arshia <arshiarezagholi1212@gmail.com>
2024-10-24 01:26:57 +0330

    added the Rbbit class

---
+++ rabbit_helper/rabbit.py
@@ -0,0 +1,12 @@
+class Rabbit:
+    def __init__(self, channel):
+        self.channel = channel
+
+    async def publish(self):
+        pass
+
+    async def consume(self):
+        pass
+
+    def queue_declare(self):
+        pass
